e7f92eada75cc28df424a3847ed7bacce84fba21
Harsh Singh <harshking6@gmail.com>
2024-02-26 23:40:30 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras.models import load_model
+import tensorflow_hub as hub
+from tensorflow.keras import preprocessing
 import time
 fig = plt.figure()
 
